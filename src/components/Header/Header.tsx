41e17fdcef746c5495f50a65e5e44aedaf7d00ee
Adrien Loup <adloup@gmail.com>
2024-05-10 14:40:25 +0200

    feat(1): init project

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import { version } from "../../../package.json";
+import { PageType } from "../../models/Page";
 import style from "./Header.module.scss";
 
-export const Header = () => {
+type HeaderProps = {
+  pages: PageType[];
+};
+
+export const Header = ({ pages }: HeaderProps) => {
   return (
     <header role="banner" className={style.header}>
       <div className={style.inner}>
         <Link className={style.name} to={"/ds/"}>
           DS<span>v{version}</span>
         </Link>
+        {JSON.stringify(pages)}
+        {/* <Menu pages={pages} /> */}
       </div>
     </header>
   );
