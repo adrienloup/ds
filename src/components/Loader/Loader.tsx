852311f87370205cacecaf0d6505dd4a34dfef67
Adrien Loup <adloup@gmail.com>
2024-05-11 05:40:56 +0200

    feat(7): add components

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
+import style from "./Loader.module.scss";
+
+export const Loader = () => {
+  const [count, setCount] = useState<number>(0);
+  const [percent, setPercent] = useState<number>(0);
+
+  const counter = useCallback(() => {
+    setCount((count) => count + 1);
+  }, []);
+
+  useEffect(() => {
+    setPercent(Math.round((count * 100) / 3));
+    if (count >= 3) {
+      document.documentElement.classList.add(`${style.loaded}`);
+      return;
+    }
+    const timeout = setInterval(counter, 500);
+    return () => clearInterval(timeout);
+  }, [counter, count]);
+
+  return (
+    <div className={style.loader}>
+      <div className={style.label}>
+        <div>
+          <span>The design system using the logic of</span> Atomic Design
+        </div>
+      </div>
+      <div className={style.percent} style={{ bottom: `${percent * 0.9}%` }}>
+        <div>{percent}%</div>
+      </div>
+    </div>
+  );
+};
