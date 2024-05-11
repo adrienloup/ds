346eba1ae735ab31de49e0672ebfef7749d1298b
Adrien Loup <adloup@gmail.com>
2024-05-11 05:41:08 +0200

    feat(1): init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DataContext } from "./contexts/DataContext";
-import { AlertProvider } from "./components/Alerts/AlertProvider";
 import { useData } from "./hooks/useData";
+import { Loader } from "./components/Loader/Loader";
+import { AlertProvider } from "./components/Alerts/AlertProvider";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Overview from "./pages/Overview";
 import "./App.scss";
 
 function App() {
@@ -12,12 +14,14 @@ function App() {
 
   return (
     <DataContext.Provider value={{ data, setData }}>
+      <Loader />
       <AlertProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/ds/" element={<Home />} />
             <Route path="/ds/login" element={<Login />} />
             <Route path="/ds/*" element={<NotFound />} />
+            <Route path="/ds/overview" element={<Overview />} />
           </Routes>
         </BrowserRouter>
       </AlertProvider>
