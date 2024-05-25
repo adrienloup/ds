35934987250a62c3a3eafa9a4d226b1f8cc9cd8a
Adrien Loup <adloup@gmail.com>
2024-05-25 08:56:50 +0200

    feat(8): add contexts

--- app/src/contexts/DataContext.tsx
+++ app/src/contexts/DataContext.tsx
@@ -2,10 +2,12 @@ import { createContext } from "react";
 import { NotificationType } from "../models/Notification";
 import { UserType } from "../models/User";
 import { DataType } from "../models/Data";
+import { SlotType } from "../models/Slot";
+import { useData } from "../hooks/useData";
 
 export const DataContext = createContext<DataType>({
   data: {
-    typology: "",
+    category: "",
     notifications: [
       { id: 1, title: "NPM package locally" },
       { id: 2, title: "Add another components" },
@@ -14,18 +16,30 @@ export const DataContext = createContext<DataType>({
     settings: {
       dir: "ltr",
       mode: "light",
+      open: false,
     },
     user: null,
   },
   setData: (data: {
-    typology: string;
+    category: string;
     notifications: NotificationType[];
     settings: {
       dir: string;
       mode: string;
+      open: boolean;
     };
     user: UserType | null;
   }) => {
     data;
   },
 });
+
+export const DataContextProvider = ({ children }: SlotType) => {
+  const { data, setData } = useData();
+
+  return (
+    <DataContext.Provider value={{ data, setData }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
--- app/src/hooks/useData.ts
+++ app/src/hooks/useData.ts
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { DataContext } from "../contexts/DataContext";
+import { DataType } from "../models/Data";
 import { useLocalStorage } from "./useLocalStorage";
 import dark from "../scss/modes/dark.module.scss";
 
 const LocalData = () => {
+  const { data } = useContext<DataType>(DataContext);
   const { getItem } = useLocalStorage();
-  return (
-    JSON.parse(getItem("_ds_y0y09_10") as string) ?? {
-      typology: "",
-      notifications: [
-        { id: 1, title: "NPM package locally" },
-        { id: 2, title: "Add another components" },
-        { id: 3, title: "Make the mobile version" },
-      ],
-      settings: {
-        open: false,
-        dir: "ltr",
-        mode: "light",
-      },
-      user: null,
-    }
-  );
+  return JSON.parse(getItem("_ds_y0y09_10") as string) ?? data;
 };
 
 export const useData = () => {
@@ -28,6 +16,7 @@ export const useData = () => {
 
   useEffect(() => {
     setItem("_ds_y0y09_10", JSON.stringify(data));
+
     data.settings.mode === "dark"
       ? document.body.classList.add(`${dark.dark}`)
       : document.body.classList.remove(`${dark.dark}`);
