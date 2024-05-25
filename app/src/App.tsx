951476a4e9d3f140c6c5bb83a023015ce3794be3
Adrien Loup <adloup@gmail.com>
2024-05-25 08:57:02 +0200

    feat(1): init project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { DataContext } from "./contexts/DataContext";
-// import { useData } from "./hooks/useData";
+import { DataContextProvider } from "./contexts/DataContext";
 import { Loader } from "./components/Loader/Loader";
 import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import NotFound from "./pages/NotFound";
 import OverviewPage from "./pages/Overview";
 import "./App.scss";
 
 function App() {
-  // const { data, setData } = useData();
-
   return (
-    // <DataContext.Provider value={{ data, setData }}>
-    <>
+    <DataContextProvider>
       <Loader />
       <BrowserRouter>
         <Routes>
           <Route path="/ds/" element={<HomePage />} />
+          <Route path="/ds/login" element={<LoginPage />} />
+          <Route path="/ds/*" element={<NotFound />} />
           <Route path="/ds/overview" element={<OverviewPage />} />
         </Routes>
       </BrowserRouter>
-      {/* </DataContext.Provider> */}
-    </>
+    </DataContextProvider>
   );
 }
 
