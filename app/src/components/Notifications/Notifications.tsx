6d2584e2f7c3acc149c4f8f823bc7998f72e97b0
Adrien Loup <adloup@gmail.com>
2024-07-06 22:16:24 +0200

    feat(21): add component on app

--- app/src/components/Notifications/Notifications.tsx
+++ app/src/components/Notifications/Notifications.tsx
@@ -10,12 +10,14 @@ export const Notifications = () => {
       {loading && <div>Loading...</div>}
       {errors && <div>Failed to fetch</div>}
       {data &&
-        data.map((post: { id: number; title: string; body: string }) => (
-          <div className={style.notification} key={post.id}>
-            <span>{post.title}</span>
-            <p>{post.body}</p>
-          </div>
-        ))}
+        data.map(
+          (notification: { id: number; title: string; body: string }) => (
+            <div className={style.notification} key={notification.id}>
+              <span>{notification.title}</span>
+              <p>{notification.body}</p>
+            </div>
+          )
+        )}
     </div>
   );
 };
