20dad52c28caa4e54144bc00578ba503494fac1f
Adrien Loup <adloup@gmail.com>
2024-07-27 12:13:47 +0200

    fix(28): fix type not assignable to pagelist component

--- app/src/components/PageList/PageList.tsx
+++ app/src/components/PageList/PageList.tsx
@@ -12,10 +12,12 @@ export const PageList = ({ list }: PageListType) => {
   let lastCategory = "";
 
   for (const page of list) {
-    if (page.category !== lastCategory) {
-      listed.push(<CardItem key={page.category} name={page.category!} />);
+    if (page.category!.value !== lastCategory) {
+      listed.push(
+        <CardItem key={page.category!.value} name={page.category!.name} />
+      );
     }
-    lastCategory = page.category!;
+    lastCategory = page.category!.value;
     listed.push(<CardItem key={page.name} name={page.name} path={page.path} />);
   }
 
