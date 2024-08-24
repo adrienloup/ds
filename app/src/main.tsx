461a32f41cc95d3d4b56d2309503525f808227c7
Adrien Loup <adloup@gmail.com>
2024-08-24 13:55:33 +0200

    feat(1): init project

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,8 +1,16 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { AllProviders } from "./utils/combineProviders";
+import { version } from "../package.json";
 import App from "./App";
 import "./i18n";
+
+console.log(
+  `%c[DS]%c${version}%c@jff`,
+  "padding: 4px 3px 3px; background: #1b1b1b; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;",
+  "padding: 4px 3px 3px; background: #fff500; font-weight: bold; color: #1b1b1b;",
+  "padding: 4px 3px 3px; background: #1b1b1b; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;"
+);
 
 ReactDOM.createRoot(document.getElementById("_ds_y0y09_10")!).render(
   <StrictMode>
