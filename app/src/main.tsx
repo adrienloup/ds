e1e6a8947712048c8990cc4d8f78a3efcdbc685a
Adrien Loup <adloup@gmail.com>
2024-08-24 14:00:33 +0200

    feat(1): init project

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -7,9 +7,9 @@ import "./i18n";
 
 console.log(
   `%c[DS]%c${version}%c@jff`,
-  "padding: 4px 3px 3px; background: #1b1b1b; border-radius: 3px 0 0 3px; font-weight: bold; color: #fff;",
+  "padding: 4px 3px 3px; background: #1b1b1b; font-weight: bold; color: #fff;",
   "padding: 4px 3px 3px; background: #fff500; font-weight: bold; color: #1b1b1b;",
-  "padding: 4px 3px 3px; background: #1b1b1b; border-radius: 0 3px 3px 0; font-weight: bold; color: #fff;"
+  "padding: 4px 3px 3px; background: #1b1b1b; font-weight: bold; color: #fff;"
 );
 
 ReactDOM.createRoot(document.getElementById("_ds_y0y09_10")!).render(
